8c3c3fc05dafa652ef7369de4481cf03a0974bcb
ezaatama <ezatama33@gmail.com>
2023-07-26 14:50:29 +0700

    implement add cart item product

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:projects_udemy1/providers/cart.dart';
 import '/providers/product.dart';
 import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String id;
-  // final String title;
-  // final String imageUrl;
+  final String id;
+  final String title;
+  final String imageUrl;
 
-  // ProductItem(
-  //   this.id,
-  //   this.title,
-  //   this.imageUrl,
-  // );
+  ProductItem(
+    this.id,
+    this.title,
+    this.imageUrl,
+  );
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
       child: GridTile(
@@ -32,12 +34,16 @@ class ProductItem extends StatelessWidget {
           ),
           footer: GridTileBar(
             backgroundColor: Colors.pink,
-            leading: IconButton(
-              onPressed: () {
-                product.toggleFavoriteStatus();
-              },
-              icon: Icon(product.isFavorite? Icons.favorite : Icons.favorite_border),
-              color: Colors.white,
+            leading: Consumer<Product>(
+              builder: (context, product, _) {
+                return IconButton(
+                  onPressed: () {
+                    product.toggleFavoriteStatus();
+                  },
+                  icon: Icon(product.isFavorite? Icons.favorite : Icons.favorite_border),
+                  color: Colors.white,
+                );
+              }
             ),
             title: Text(
               product.title,
@@ -45,7 +51,9 @@ class ProductItem extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             trailing: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product.id, product.price, product.title);
+              },
               icon: Icon(Icons.add_shopping_cart),
               color: Colors.white,
             ),
