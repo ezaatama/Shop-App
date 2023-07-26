505d4ae5ea4a679a4f815916cc8d01a0c45457ac
ezaatama <ezatama33@gmail.com>
2023-07-26 14:48:46 +0700

    change multiprovider on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:projects_udemy1/providers/cart.dart';
 import 'package:projects_udemy1/providers/products.dart';
 import 'package:provider/provider.dart';
 import '/providers/products.dart';
@@ -8,32 +9,30 @@ import '/screens/products_overview_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((value) =>
-  runApp(MyApp()));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(MyApp()));
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
- 
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (ctx) => Products()),
+        ChangeNotifierProvider(create: (ctx) => Cart()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Nunito',
         ),
         home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
-        },
+        routes: {ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()},
       ),
     );
   }
