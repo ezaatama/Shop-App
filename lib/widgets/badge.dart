83a0bdd8e44219e960bca075b3328cb70b467c55
ezaatama <ezatama33@gmail.com>
2023-07-26 14:49:07 +0700

    badges widget

---
+++ lib/widgets/badge.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: unnecessary_null_comparison
+
+import 'package:flutter/material.dart';
+
+class Badges extends StatelessWidget {
+  const Badges({Key? key, required this.child, required this.value, this.color}) : super(key: key);
+
+  final Widget child;
+  final String value;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        child,
+        Positioned(
+          right: 8,
+          top: 8,
+          child: Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: color != null ? color : Theme.of(context).primaryColor
+            ),
+            constraints: BoxConstraints(
+              minWidth: 16,
+              minHeight: 16,
+            ),
+            child: Text(
+              value,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10
+              ),
+            ),
+          ))
+      ],
+    );
+  }
+}
