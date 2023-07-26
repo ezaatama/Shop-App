c05c15bca3b5e787cfc05801f96e21d0fa770472
ezaatama <ezatama33@gmail.com>
2023-07-26 14:49:25 +0700

    show favorites in product grid

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -4,20 +4,24 @@ import 'package:provider/provider.dart';
 import '/widgets/product_item.dart';
 
 class ProductGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductGrid({required this.showFavs});
 
   @override
   Widget build(BuildContext context) {
   final productsData = Provider.of<Products>(context);
-  final products = productsData.items;
+  final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (c) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        // create: (c) => products[i],
+        value: products[i],
         child: ProductItem(
-            // products[i].id,
-            // products[i].title, 
-            // products[i].imageUrl
-          ),
+            products[i].id,
+            products[i].title, 
+            products[i].imageUrl
+          )
       ),
       itemCount: products.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
