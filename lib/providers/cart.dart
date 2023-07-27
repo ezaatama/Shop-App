2f0f8c24a8f2d31a6cfd5f96e4b2ce5a3f060a09
ezaatama <ezatama33@gmail.com>
2023-07-27 14:45:11 +0700

    cart item (add, update, remove, total amount)

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -13,21 +13,41 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, value) {
+      total += value.price * value.quantity!;
+    });
+
+    return total;
+  }
+
   void addItem(String productId, double price, String title) {
     if (_items.containsKey(productId)) {
-      _items.update(
-          productId,
-          (existingCartItem) => CartItem(
-              id: existingCartItem.id,
-              title: existingCartItem.title,
-              price: existingCartItem.price,
-              quantity: existingCartItem.quantity! + 1));
+      _items.update(productId, (existingCartItem) {
+        print(existingCartItem.quantity);
+        return CartItem(
+            id: existingCartItem.id,
+            title: existingCartItem.title,
+            price: existingCartItem.price,
+            quantity: existingCartItem.quantity! + 1);
+      });
     } else {
       _items.putIfAbsent(
           productId,
           () => CartItem(
               id: DateTime.now().toString(), title: title, price: price));
     }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
     notifyListeners();
   }
 }
@@ -41,7 +61,7 @@ class CartItem {
   CartItem({
     required this.id,
     required this.title,
-    this.quantity,
+    this.quantity = 1,
     required this.price,
   });
 }
